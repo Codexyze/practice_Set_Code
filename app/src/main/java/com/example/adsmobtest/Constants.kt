df2c4086bd21366aa4d2439d910ea01ee610b55f
Akshay Sarapure <akshaysarapure@gmail.com>
2025-02-15 12:18:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/adsmobtest/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.adsmobtest
+
+object Constants {
+    const val ADDMOBID="ca-app-pub-3150717277102846~3759972568"
+}
---
+++ app/src/main/java/com/example/adsmobtest/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.adsmobtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.adsmobtest.AdScreens.BannerAds
+import com.example.adsmobtest.ui.theme.AdsMobTestTheme
+import com.google.android.gms.ads.MobileAds
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
+        backgroundScope.launch {
+            // Initialize the Google Mobile Ads SDK on a background thread.
+            MobileAds.initialize(this@MainActivity) {}
+        }
+        setContent {
+
+            AdsMobTestTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                   Box(modifier = Modifier.padding(innerPadding)){
+                       Text("Hello world")
+
+                      // BannerAds()
+                   }
+                }
+            }
+        }
+    }
+}
