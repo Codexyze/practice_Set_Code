3d95f14b449f8953e2c436b42117c4284e2a5108
Akshay Sarapure <akshaysarapure@gmail.com>
2025-06-06 11:48:54 +0530

    feat(MultipleScreen):Support for multiple screen size

--- MutiScreenSize/app/src/main/java/com/example/mutiscreensupport/MainActivity.kt
+++ MutiScreenSize/app/src/main/java/com/example/mutiscreensupport/MainActivity.kt
@@ -4,44 +4,128 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mutiscreensupport.ui.theme.MutiScreenSupportTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MutiScreenSupportTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val windowInfo = rememberWindowInfo()
+                if(windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        // List 1
+                        items(10) {
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Cyan)
+                                    .padding(16.dp)
+                            )
+                        }
+                        // List 2
+                        items(10) {
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Green)
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
+                } else {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            // List 1
+                            items(10) {
+                                Text(
+                                    text = "Item $it",
+                                    fontSize = 25.sp,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.Green)
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            // List 1
+                            items(10) {
+                                Text(
+                                    text = "Item $it",
+                                    fontSize = 25.sp,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.Cyan)
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun rememberWindowInfo(): WindowInfo {
+    val configuration = LocalConfiguration.current
+    return WindowInfo(
+        screenWidthInfo = when {
+            configuration.screenWidthDp < 600 -> WindowInfo.WindowType.Compact
+            configuration.screenWidthDp < 840 -> WindowInfo.WindowType.Medium
+            else -> WindowInfo.WindowType.Expanded
+        },
+        screenHeightInfo = when {
+            configuration.screenHeightDp < 480 -> WindowInfo.WindowType.Compact
+            configuration.screenHeightDp < 900 -> WindowInfo.WindowType.Medium
+            else -> WindowInfo.WindowType.Expanded
+        },
+        screenWidth = configuration.screenWidthDp.dp,
+        screenHeight = configuration.screenHeightDp.dp
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MutiScreenSupportTheme {
-        Greeting("Android")
+data class WindowInfo(
+    val screenWidthInfo: WindowType,
+    val screenHeightInfo: WindowType,
+    val screenWidth: Dp,
+    val screenHeight: Dp
+) {
+    sealed class WindowType {
+        object Compact: WindowType()
+        object Medium: WindowType()
+        object Expanded: WindowType()
     }
 }
